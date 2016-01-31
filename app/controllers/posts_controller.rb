7f64038dea48ea46f344b400e9ecaefc9a2ee25d
John Jack Franji <jeanjack.franji@gmail.com>
2016-01-31 19:22:25 +0100

    Changes to Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,14 @@
 class PostsController < ApplicationController
-  before_filter :authorize
+
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.paginate(:page => params[:page], :per_page => 10)
-    #@posts = Post.all
+
+      @Offers = Post.where("forSale = 'sell' ").order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+      @OfferExchanges = Post.where("forSale = 'exchange' ").order("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+
   end
 
   # GET /posts/1
@@ -16,16 +18,19 @@ class PostsController < ApplicationController
 
   # GET /posts/new
   def new
+    validate_if_user_logged_in
     @post = Post.new
   end
 
   # GET /posts/1/edit
   def edit
+    validate_if_user_logged_in
   end
 
   # POST /posts
   # POST /posts.json
   def create
+    validate_if_user_logged_in
     @post = Post.new(post_params)
 
     respond_to do |format|
@@ -43,6 +48,7 @@ class PostsController < ApplicationController
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
   def update
+    validate_if_user_logged_in
     respond_to do |format|
       if @post.update(post_params)
         format.html { redirect_to @post }
@@ -57,6 +63,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
+    validate_if_user_logged_in
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_url}
@@ -65,6 +72,13 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def validate_if_user_logged_in
+    if !check_login_state
+      redirect_to (login_path + '?From='+ request.fullpath)
+    end
+  end
+
   # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
