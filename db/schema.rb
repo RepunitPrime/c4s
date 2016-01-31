821fbded0f72004b0b4b9ed3ca62ac05fea90bf6
Neetha Baliga <neetha.baligabantwal@zlw-ima.rwth-aachen.de>
2016-01-31 18:23:41 +0100

    Phone Number changes to DB

--- db/schema.rb
+++ db/schema.rb
@@ -125,6 +125,7 @@ ActiveRecord::Schema.define(version: 201601197051563) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "phone"
     t.string   "profile_pic_file_name"
     t.string   "profile_pic_content_type"
     t.integer  "profile_pic_file_size"
