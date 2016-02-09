66729e3f6ee70caf2a812e2c9234a1eca92a59f5
John Jack Franji <jeanjack.franji@gmail.com>
2016-02-09 01:48:03 +0100

    Bug Fixes

--- app/models/article_attachment.rb
+++ app/models/article_attachment.rb
@@ -5,7 +5,7 @@ class ArticleAttachment < ActiveRecord::Base
                     :path => ":rails_root/public/files/:id/:filename",
                     :url  => "/files/:id/:filename"
 
-  validates_attachment_file_name :attach_file, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/,/docx\Z/,/doc\Z/,/pdf\Z/]
+  validates_attachment_file_name :attach_file, :matches => [/txt\Z/,/ico\Z/,/log\Z/,/png\Z/, /jpe?g\Z/, /gif\Z/,/docx\Z/,/doc\Z/,/pdf\Z/]
 
   validates :attach_file,
             attachment_size: { less_than: 5.megabytes }
